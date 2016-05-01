e33573effe7b882a78aa954347fd6992b7ee9220
trikitrok <trikitrok@gmail.com>
2016-05-01 02:41:08 +0200

    refactor: remove obsolete field

--- src/main/java/com/dodevjutsu/kata/yatzy/Yatzy.java
+++ src/main/java/com/dodevjutsu/kata/yatzy/Yatzy.java
@@ -8,14 +8,12 @@ import static com.dodevjutsu.kata.yatzy.Category.Ones;
 
 public class Yatzy {
     private static final int NUMBER_OF_RERUNS = 2;
-    private final InputReader inputReader;
     private final DiceRoller diceRoller;
     private final Notifier notifier;
     private final DiceReruns diceReruns;
     private List<Category> categories = Arrays.asList(Ones, Twos);
 
     public Yatzy(DieRoller dieRoller, Notifier notifier, InputReader inputReader) {
-        this.inputReader = inputReader;
         this.notifier = notifier;
         this.diceRoller = new DiceRoller(dieRoller);
         this.diceReruns = new DiceReruns(NUMBER_OF_RERUNS, diceRoller, notifier, inputReader);
