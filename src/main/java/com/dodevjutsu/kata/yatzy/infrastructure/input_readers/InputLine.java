6ae433d25126addbbb55eea7a25d421fa0df9b43
trikitrok <trikitrok@gmail.com>
2016-05-11 00:54:16 +0200

    refactor: simplify dice indexes extraction

--- src/main/java/com/dodevjutsu/kata/yatzy/infrastructure/input_readers/InputLine.java
+++ src/main/java/com/dodevjutsu/kata/yatzy/infrastructure/input_readers/InputLine.java
@@ -1,29 +1,35 @@
 package com.dodevjutsu.kata.yatzy.infrastructure.input_readers;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class InputLine {
-    private final String content;
+    private final String line;
+    private static final Map<String, Integer> indexesByDieRepresentation = new HashMap<>();
 
-    public InputLine(String content) {
-        this.content = content;
+    static {
+        indexesByDieRepresentation.put("D1", 0);
+        indexesByDieRepresentation.put("D2", 1);
+        indexesByDieRepresentation.put("D3", 2);
+        indexesByDieRepresentation.put("D4", 3);
+        indexesByDieRepresentation.put("D5", 4);
+    }
+
+    public InputLine(String line) {
+        this.line = line;
     }
 
     public List<Integer> extractDiceIndexes() {
-        String[] tokens = content.split(" ");
-        return Stream.of(tokens)
-            .map(this::toDiceNumber)
-            .map(this::toDiceIndex)
+        String[] dieRepresentations = line.split(" ");
+        return Stream.of(dieRepresentations)
+            .map(this::dieRepresentationToIndex)
             .collect(Collectors.toList());
     }
 
-    private int toDiceIndex(Integer diceNumber) {
-        return diceNumber - 1;
-    }
-
-    private int toDiceNumber(String token) {
-        return Integer.parseInt(token.substring(1));
+    private int dieRepresentationToIndex(String token) {
+        return indexesByDieRepresentation.get(token);
     }
 }
