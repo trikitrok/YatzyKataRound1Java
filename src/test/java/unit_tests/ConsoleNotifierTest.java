da7947f1a105811c3b15c466a320172e987dd592
trikitrok <trikitrok@gmail.com>
2016-05-11 02:03:30 +0200

    refactor: remove obsolete code

--- src/test/java/unit_tests/ConsoleNotifierTest.java
+++ src/test/java/unit_tests/ConsoleNotifierTest.java
@@ -50,13 +50,6 @@ public class ConsoleNotifierTest {
 
     @Test
     public void asks_user_to_input_dice_to_reroll() {
-        DieRoller notNeededDieRoller = null;
-        Notifier notNeededNotifier = null;
-        InputReader notNeededInputReader = null;
-        final int NUM_RERUNS_SO_FAR = 1;
-        DiceRerun diceRerun = new DiceRerun(
-            NUM_RERUNS_SO_FAR, notNeededDieRoller, notNeededNotifier, notNeededInputReader
-        );
         context.checking(new Expectations() {{
             oneOf(console).print("[1] Dice to re-run:");
         }});
